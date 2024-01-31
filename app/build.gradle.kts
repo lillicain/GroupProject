239174733d27bcc1afd918045582da8e86a8da06
lilli <lilliancrabbit@gmail.com>
2024-01-31 14:10:35 -0700

    Camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,10 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
