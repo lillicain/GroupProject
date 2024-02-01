22a958409532101b396d7fc07433dd2df3d4cf83
lilli <lilliancrabbit@gmail.com>
2024-02-01 11:06:04 -0700

    Camera is in main :(

--- app/src/main/java/com/example/groupproject/MainActivity.kt
+++ app/src/main/java/com/example/groupproject/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.groupproject
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCaptureSession.*
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.hardware.camera2.CaptureRequest
+import android.media.ImageReader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.os.HandlerThread
+import android.provider.ContactsContract.CommonDataKinds.Im
+import android.view.Surface
 import android.view.TextureView
+import android.widget.Button
+import android.widget.Toast
 import androidx.camera.lifecycle.ProcessCameraProvider
 import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     lateinit var cameraManager: CameraManager
@@ -23,13 +34,24 @@ class MainActivity : AppCompatActivity() {
     lateinit var captureRequest: CaptureRequest
     lateinit var handler: Handler
     lateinit var handlerThread: HandlerThread
-    lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    lateinit var capReq: CaptureRequest.Builder
+    lateinit var imageReader: ImageReader
 
+//    lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraDevice.close()
+        handler.removeCallbacksAndMessages(null)
+        handlerThread.quitSafely()
+    }
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getPermissions()
+
         textureView = findViewById(R.id.textureView)
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         handlerThread = HandlerThread("videoThread")
@@ -50,14 +72,59 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
             }
+        }
 
+        imageReader = ImageReader.newInstance(1080, 1920, ImageFormat.JPEG, 1)
+        imageReader.setOnImageAvailableListener(object: ImageReader.OnImageAvailableListener {
+            override fun onImageAvailable(reader: ImageReader?) {
+                var image = reader?.acquireLatestImage()
+                var buffer = image!!.planes[0].buffer
+                var bytes = ByteArray(buffer.remaining())
+                buffer.get(bytes)
+                var file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "img.jpeg")
+                var opStream = FileOutputStream(file)
+                opStream.write(bytes)
+                opStream.close()
+                image.close()
+
+
+
+                Toast.makeText(this@MainActivity, "image captured", Toast.LENGTH_SHORT).show()
+            }
+        }, handler)
+
+        findViewById<Button>(R.id.capture).apply {
+            setOnClickListener {
+                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+                capReq.addTarget(imageReader.surface)
+                cameraCaptureSession.capture(capReq.build(), null, null)
+            }
         }
     }
 
-    fun open_camera() {
-        cameraManager.openCamera(cameraManager.cameraIdList[0], object : CameraDevice.StateCallback() {
-            override fun onOpened(camera: CameraDevice) {
+    @SuppressLint("MissingPermission")
 
+    fun open_camera() {
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object: CameraDevice.StateCallback() {
+
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
+
+                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                var surface = Surface(textureView.surfaceTexture)
+                capReq.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface, imageReader.surface), object: CameraCaptureSession.StateCallback {
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        cameraCaptureSession = session
+                        cameraCaptureSession.setRepeatingRequest(capReq.build(), null, null)
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+
+                    }
+
+                }, handler)
             }
 
             override fun onDisconnected(camera: CameraDevice) {
@@ -65,11 +132,13 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onError(camera: CameraDevice, error: Int) {
-         
+
             }
 
-        }
+        }, handler)
     }
+
+
 
     fun getPermissions() {
         var permissionList = mutableListOf<String>()
