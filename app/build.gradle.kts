673d89a5242d0f91da4e6e8cff0ba50ab3f48cd6
lilli <lilliancrabbit@gmail.com>
2024-02-01 13:58:37 -0700

    No errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,9 +62,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("androidx.camera.camera2:camera-camera2:1.1.0")
-    implementation("androidx.camera.lifecycle:lifecycle-camera:1.1.0")
-
 
     implementation("androidx.camera:camera-core:1.3.1")
     implementation("androidx.camera:camera-camera2:1.3.1")
