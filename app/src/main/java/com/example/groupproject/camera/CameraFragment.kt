ee472753ce0d5f7278f13ee66ed37cbaea9e1adb
lilli <lilliancrabbit@gmail.com>
2024-02-01 17:33:43 -0700

    1

--- app/src/main/java/com/example/groupproject/camera/CameraFragment.kt
+++ app/src/main/java/com/example/groupproject/camera/CameraFragment.kt
@@ -12,104 +12,104 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.example.groupproject.MainActivity
-import com.example.groupproject.Manifest
+
 import com.example.groupproject.R
 import com.example.groupproject.R.*
 import com.example.groupproject.R.id.*
 
-class CameraFragment: AppCompatActivity() {
-    private lateinit var previewView: CameraFragment
-    private lateinit var cameraProvider: ProcessCameraProvider
-    private lateinit var cameraSelector: CameraSelector
-    private lateinit var preview: Preview
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(layout.fragment_camera)
-
-       //previewView = findViewById(id.preview)
-
-
-        // Initialize CameraX
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
-            )
-        }
-    }
-
-    private fun allPermissionsGranted(): Boolean {
-        // Check for camera permissions
-        for (permission in REQUIRED_PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(
-                    this, permission
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                return false
-            }
-        }
-        return true
-    }
-
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture.addListener({
-            // CameraProvider is now guaranteed to be available
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Set up the preview use case
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider {
-                        preview
-                    }
-                }
-
-            // Select back camera
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind any existing use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                    this as LifecycleOwner, cameraSelector, preview
-                )
-
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(this))
-    }
-
-    // Handle permission request response
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(
-                    this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                finish()
-            }
-        }
-    }
-
-    companion object {
-        private const val TAG = "YourCameraActivity"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
-    }
+class CameraFragment {
+//    private lateinit var previewView: CameraFragment
+//    private lateinit var cameraProvider: ProcessCameraProvider
+//    private lateinit var cameraSelector: CameraSelector
+//    private lateinit var preview: Preview
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//       //previewView = findViewById(id.preview)
+//
+//
+//        // Initialize CameraX
+//        if (allPermissionsGranted()) {
+//            startCamera()
+//        } else {
+//            ActivityCompat.requestPermissions(
+//                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+//            )
+//        }
+//    }
+//
+//    private fun allPermissionsGranted(): Boolean {
+//        // Check for camera permissions
+//        for (permission in REQUIRED_PERMISSIONS) {
+//            if (ContextCompat.checkSelfPermission(
+//                    this, permission
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                return false
+//            }
+//        }
+//        return true
+//    }
+//
+//    private fun startCamera() {
+//        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+//
+//        cameraProviderFuture.addListener({
+//            // CameraProvider is now guaranteed to be available
+//            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+//
+//            // Set up the preview use case
+//            val preview = Preview.Builder()
+//                .build()
+//                .also {
+//                    it.setSurfaceProvider {
+//                        preview
+//                    }
+//                }
+//
+//            // Select back camera
+//            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+//
+//            try {
+//                // Unbind any existing use cases before rebinding
+//                cameraProvider.unbindAll()
+//
+//                // Bind use cases to camera
+//                cameraProvider.bindToLifecycle(
+//                    this as LifecycleOwner, cameraSelector, preview
+//                )
+//
+//            } catch (exc: Exception) {
+//                Log.e(TAG, "Use case binding failed", exc)
+//            }
+//
+//        }, ContextCompat.getMainExecutor(this))
+//    }
+//
+//    // Handle permission request response
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+//            if (allPermissionsGranted()) {
+//                startCamera()
+//            } else {
+//                Toast.makeText(
+//                    this,
+//                    "Permissions not granted by the user.",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//                finish()
+//            }
+//        }
+//    }
+//
+//    companion object {
+//        private const val TAG = "YourCameraActivity"
+//        private const val REQUEST_CODE_PERMISSIONS = 10
+//        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+//    }
 
 }
