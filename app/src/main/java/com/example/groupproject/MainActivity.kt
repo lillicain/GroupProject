dae441ad2c0ee255534f6e507b8b30788463011f
lilli <lilliancrabbit@gmail.com>
2024-02-03 18:19:55 -0700

    1

--- app/src/main/java/com/example/groupproject/MainActivity.kt
+++ app/src/main/java/com/example/groupproject/MainActivity.kt
@@ -16,6 +16,7 @@ import android.os.Environment
 import android.os.Handler
 import android.os.HandlerThread
 import android.view.Surface
+import android.view.SurfaceView
 import android.view.TextureView
 import android.widget.Button
 import android.widget.Toast
@@ -24,32 +25,25 @@ import java.io.File
 import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
-//    lateinit var cameraManager: CameraManager
-//    lateinit var textureView: TextureView
-//    lateinit var cameraCaptureSession: CameraCaptureSession
-//    lateinit var cameraDevice: CameraDevice
-//    lateinit var captureRequest: CaptureRequest
-//    lateinit var handler: Handler
-//    lateinit var handlerThread: HandlerThread
-//    lateinit var capReq: CaptureRequest.Builder
-//    lateinit var imageReader: ImageReader
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        cameraDevice.close()
-//        handler.removeCallbacksAndMessages(null)
-//        handlerThread.quitSafely()
-//    }
+    lateinit var cameraManager: CameraManager
+    lateinit var textureView: TextureView
+    lateinit var cameraCaptureSession: CameraCaptureSession
+    lateinit var cameraDevice: CameraDevice
+    lateinit var captureRequest: CaptureRequest
+    lateinit var handler: Handler
+    lateinit var handlerThread: HandlerThread
+    lateinit var capReq: CaptureRequest.Builder
+    lateinit var imageReader: ImageReader
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        getPermissions()
-    }
+//        getPermissions()
 
-    //        textureView = textureView.findViewById(id.textureView)
+//            textureView = textureView.findViewById(id.textureView)
 //        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 //        handlerThread = HandlerThread("videoThread")
 //        handlerThread.start()
@@ -90,13 +84,13 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }, handler)
 //
-//        findViewById<Button>(id.capture).apply {
-//            setOnClickListener {
-//                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
-//                capReq.addTarget(imageReader.surface)
-//                cameraCaptureSession.capture(capReq.build(), null, null)
-//            }
-//        }
+////        findViewById<Button>(capture).apply {
+////            setOnClickListener {
+////                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+////                capReq.addTarget(imageReader.surface)
+////                cameraCaptureSession.capture(capReq.build(), null, null)
+////            }
+////        }
 //    }
 //
 //    @SuppressLint("MissingPermission")
@@ -130,37 +124,43 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }, handler)
 //    }
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        cameraDevice.close()
+//        handler.removeCallbacksAndMessages(null)
+//        handlerThread.quitSafely()
+//    }
 //
-    fun getPermissions() {
-        var permissionList = mutableListOf<String>()
-
-        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) permissionList.add(
-            android.Manifest.permission.CAMERA
-        )
-        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) permissionList.add(
-            android.Manifest.permission.READ_EXTERNAL_STORAGE
-        )
-        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) permissionList.add(
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
-
-        if (permissionList.size > 0) {
-            requestPermissions(permissionList.toTypedArray(), 101)
-
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        grantResults.forEach {
-            if (it != PackageManager.PERMISSION_GRANTED) {
-                getPermissions()
-            }
-        }
+//    fun getPermissions() {
+//        var permissionList = mutableListOf<String>()
+//
+//        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) permissionList.add(
+//            android.Manifest.permission.CAMERA
+//        )
+//        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) permissionList.add(
+//            android.Manifest.permission.READ_EXTERNAL_STORAGE
+//        )
+//        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) permissionList.add(
+//            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+//        )
+//
+//        if (permissionList.size > 0) {
+//            requestPermissions(permissionList.toTypedArray(), 101)
+//
+//        }
+//    }
+//
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        grantResults.forEach {
+//            if (it != PackageManager.PERMISSION_GRANTED) {
+//                getPermissions()
+//            }
+//        }
     }
 }
 
