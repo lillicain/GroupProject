927f1dc974c0991c1340427ee6d3c76f4f48ddef
lilli <lilliancrabbit@gmail.com>
2024-02-04 09:11:02 -0700

    Fragment

--- app/src/main/java/com/example/groupproject/camera/Util.kt
+++ app/src/main/java/com/example/groupproject/camera/Util.kt
@@ -1,5 +1,6 @@
 package com.example.groupproject.camera
 
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
@@ -8,7 +9,7 @@ import android.view.View
 import android.widget.Toast
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-fun appSettingOpen(context: CameraFragment) {
+fun appSettingOpen(context: Context) {
     Toast.makeText(context, "Enable All Permissions", Toast.LENGTH_LONG).show()
 
     val settingIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
@@ -17,7 +18,7 @@ fun appSettingOpen(context: CameraFragment) {
     context.startActivity(settingIntent)
 }
 
-fun warningPermissionDialog(context: CameraFragment, listener: DialogInterface.OnClickListener){
+fun warningPermissionDialog(context: Context, listener: DialogInterface.OnClickListener){
     MaterialAlertDialogBuilder(context)
         .setMessage("All Permission are Required for this app")
         .setCancelable(false)
