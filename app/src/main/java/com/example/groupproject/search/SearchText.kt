24029037f86a996a79f0b2db220cd55a5d89b7be
lilli <lilliancrabbit@gmail.com>
2024-02-05 16:08:39 -0700

    SearchUtil

--- app/src/main/java/com/example/groupproject/search/SearchText.kt
+++ app/src/main/java/com/example/groupproject/search/SearchText.kt
@@ -14,19 +14,3 @@ data class SearchText(
     val startIndex: Int?,
     val endIndex: Int?
 ) : Parcelable
-
-val client = OkHttpClient()
-
-val mediaType = MediaType.parse("application/json")
-val body = RequestBody.create(mediaType, "{\"style\":\"general\"}")
-val request = Request.Builder()
-    .url("https://api.ai21.com/studio/v1/paraphrase")
-    .post(body)
-    .addHeader("accept", "application/json")
-    .addHeader("content-type", "application/json")
-    .addHeader("Authorization", "Bearer YOUR_API_KEY")
-    .build()
-
-val response = client.newCall(request).execute()
-
-val key = "6i8rh0iJ3C0Rc9fRcFhX2LarE4oVwa3N"
