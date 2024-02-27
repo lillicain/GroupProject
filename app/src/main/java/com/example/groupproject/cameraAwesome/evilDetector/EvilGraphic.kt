11fb88ae96dac14fb1e58b6d0657388b10ba62e4
lilli <lilliancrabbit@gmail.com>
2024-02-27 15:04:12 -0700

    1

--- app/src/main/java/com/example/groupproject/cameraAwesome/evilDetector/EvilGraphic.kt
+++ app/src/main/java/com/example/groupproject/cameraAwesome/evilDetector/EvilGraphic.kt
@@ -4,8 +4,6 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
-import android.graphics.Shader
-import androidx.compose.ui.graphics.RadialGradientShader
 import com.example.groupproject.cameraAwesome.facedetector.FaceGraphic
 import com.example.groupproject.preference.GraphicOverlay
 import com.google.mlkit.vision.face.Face
@@ -27,7 +25,6 @@ class EvilGraphic constructor(overlay: GraphicOverlay?, private val face: Face) 
     init {
         val selectedColor = Color.RED
         facePositionPaint = Paint()
-        facePositionPaint.isFakeBoldText = true
         facePositionPaint.color = selectedColor
 
         for (i in 0 until numColors) {
@@ -192,7 +189,7 @@ class EvilGraphic constructor(overlay: GraphicOverlay?, private val face: Face) 
         drawFaceLandmark(canvas, FaceLandmark.LEFT_EYE)
         drawFaceLandmark(canvas, FaceLandmark.RIGHT_EYE)
         drawFaceLandmark(canvas, FaceLandmark.LEFT_CHEEK)
-        drawFaceLandmark(canvas, FaceLandmark.RIGHT_CHEEK)
+        drawFaceLandmark(canvas, FaceLandmarxk.RIGHT_CHEEK)
         drawFaceLandmark(canvas, FaceLandmark.MOUTH_BOTTOM)
     }
 
